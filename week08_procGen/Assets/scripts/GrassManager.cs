d6d37c1fdbe79119680164c6cbba2b1044813b22
Robert Yang <yang.robert.w@gmail.com>
2017-10-24 20:24:47 -0400

    week08 intro to proc gen (GrassManager.cs, TenPrint.cs)

---
+++ week08_procGen/Assets/scripts/GrassManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassManager : MonoBehaviour {
+
+	public Transform grassPrefab; // assign in Inspector
+	int grassCount = 0; // count how many blades of grass I've instantiated
+
+	// Update is called once per frame
+	void Update () {
+		// if we have planted less than 1000 blades of grass...
+		if( grassCount < 1000 ) {
+			// pick a random position
+			Vector3 randomPosition = new Vector3( Random.Range(-5f, 5f), 0f, Random.Range(-5f, 5f) );
+			// then instantiate another blade of grass, at randomPosition, with rotation of 0,0,0
+			Instantiate( grassPrefab, randomPosition, Quaternion.Euler(0,Random.Range(0f, 360f),0) );
+			// and increment the counter
+			grassCount++; 
+		}
+	}
+}
---
+++ week08_procGen/Assets/scripts/TenPrint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TenPrint : MonoBehaviour {
+
+	public GameObject linePrefab; // assign in Inspector
+
+	// Use this for initialization
+	void Start () {
+		Random.InitState( 1 ); // set our Random seed to "1" to guarantee the same sequence of random numbers
+
+		for( int y = 0; y < 50; y++ ) { // vertical for() loop, move the row downwards
+			for( int x = 0; x < 50; x++ ) { // horizontal for() loop, fill in each row
+				// calculate where we want to instantiate the line in the grid
+				Vector3 spawnPosition = new Vector3( x * 5, y * 5, 0 );
+				// randomly rotate the linePrefab 45 degrees left or right
+				Vector3 spawnAngles = Vector3.zero;
+				if( Random.Range( 0, 100) > 50 ) {
+					spawnAngles = new Vector3( 0f, 0f, 45f );
+				} else {
+					spawnAngles = new Vector3( 0f, 0f, -45f );
+				}
+				// actually instantiate at the spawnPosition
+				GameObject myNewClone = (GameObject)Instantiate( linePrefab, spawnPosition, Quaternion.Euler( spawnAngles ) );
+				// change clone's color to a random color
+				myNewClone.GetComponent<Renderer>().material.color = Random.ColorHSV();
+				// how would we change the clone's scale / size?
+				myNewClone.transform.localScale *= Random.Range(0.5f, 2f); 
+			}
+		}
+	}
+	
+
+}
