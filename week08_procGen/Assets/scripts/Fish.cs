41cff3c8e9f4db4ec13980b3f133c2f67a1ef999
Robert Yang <yang.robert.w@gmail.com>
2017-10-26 10:55:08 -0400

    week08b -- more proc gen, FishDemoScene (Fish.cs + FishGod.cs)

---
+++ week08_procGen/Assets/scripts/Fish.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO LIST:
+// X make fish look at its destination
+// X make it stop vibrating when it reaches its destination
+// X visualize a debug line to show where the fish is going
+
+public class Fish : MonoBehaviour {
+
+	public float speed = 5f;
+	public Vector3 destination; // the fish will always swim towards its destination
+	
+	// Update is called once per frame
+	void Update () {
+		// make the fish always swim towards its destination
+		Vector3 moveDir = destination - transform.position; // vector from A to B = B - A
+		Debug.DrawLine( transform.position, destination, Color.yellow ); // visualize path in Scene tab
+
+		// normalize the vector so that the fish doesn't teleport
+		if( moveDir.magnitude > 1f ) { // but only normalize if it's far from its destination!
+			moveDir = moveDir.normalized; 
+		}
+		// moveDir = Vector3.Normalize( moveDir ); // this does the same thing too
+
+		// actually move the fish now!
+		transform.position += moveDir * speed * Time.deltaTime; 
+
+		// instantaneously snap the fish to look at its destination
+		// transform.LookAt( destination ); // instant!
+
+		// smoothly, slowly, turn the fish toward its destination
+		transform.forward = Vector3.Lerp( transform.forward, moveDir, speed * Time.deltaTime );
+	}
+}
---
+++ week08_procGen/Assets/scripts/FishGod.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishGod : MonoBehaviour {
+
+	// prefab to instantiate
+	public Fish myFishPrefab; // assign in Inspector; only objects with a Fish script are allowed!
+
+	public int maxFishCount = 100; // how many Fish the FishGod should make?
+	List<Fish> myFishList = new List<Fish>(); // a list of all the fish we spawn
+
+
+	// Use this for initialization
+	void Start () {
+		int currentFishCount = 0;
+		while( currentFishCount < maxFishCount ) {
+			// instantiate the fish
+			Fish newFishClone = (Fish)Instantiate( myFishPrefab, Random.insideUnitSphere * 10f, Random.rotation );
+			myFishList.Add( newFishClone ); // remember the fish in the list
+			// increment fish count
+			currentFishCount++; 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// if we press SPACEBAR, tell all the fish to move somewhere else
+		if( Input.GetKeyDown( KeyCode.Space ) ) {
+			Vector3 randomDestination = Random.insideUnitSphere * 20f;
+			foreach( Fish thisFish in myFishList ) { // "foreach" is good for lists / arrays
+				thisFish.destination = randomDestination + Random.insideUnitSphere * 5f;
+			}
+		}
+	}
+}
