5b74daf98a10f428bafcea45dd314e94dee80da6
Robert Yang <yang.robert.w@gmail.com>
2017-11-07 12:07:29 -0500

    week10a: coroutine intro (CoroutineDemo.cs, MoveCoroutine.cs)

---
+++ week10_coroutines/Assets/scripts/CoroutineDemo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineDemo : MonoBehaviour {
+
+	public Transform cube; // assign in Inspector
+
+	// Use this for initialization
+	void Start () {
+		// begin the coroutine
+		// MyFirstCoroutine(); // WRONG, DO NOT DO THIS
+		StartCoroutine( MyFirstCoroutine() ); // RIGHT WAY TO START A COROUTINE!
+	}
+	
+	IEnumerator MyFirstCoroutine () {
+		Debug.Log( "coroutine started!" );
+		yield return 0; // tells coroutine to wait 1 frame / pause 1 frame before continuing
+		Debug.Log( "waited 1 frame! now continuing...");
+
+		yield return 0; // waits for 1 frame...
+		yield return null; // waits for a second frame, now we waited for 2 frames
+		Debug.Log( "waited 2 more frames! now continuing..." );
+
+		// if you want to wait for a number of seconds, use WaitForSeconds
+		yield return new WaitForSeconds( 4.20f ); // will wait for 4.2 seconds
+		Debug.Log( "waited for 4.2 seconds!" );
+		// the flaw of that method: you cannot do anything during those 4.2 seconds
+
+		// turn cube on and off for a period of time
+		float t = 0f; // declare a time counter variable
+		while( t < 6.9f ) { // as long as "t" is less than 6.9 seconds, keep repeating...
+			t += Time.deltaTime; // add the duration of the frame to "t"... after 6.9s, t = 6.9s
+			cube.gameObject.SetActive( !cube.gameObject.activeInHierarchy ); // toggle the cube
+			cube.Rotate( 0f, 90f * Time.deltaTime, 0f ); // make it spin too
+			yield return 0; // wait a frame at the end of each loop iteration
+		}
+		// make sure the cube is off after everything is done
+		cube.gameObject.SetActive( false );
+
+	}
+
+}
---
+++ week10_coroutines/Assets/scripts/MoveCoroutine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCoroutine : MonoBehaviour {
+
+	bool isAlreadyMoving = false; // track whether the MoveForwardCoroutine is already running
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetKeyDown( KeyCode.Space ) ) {
+			StartCoroutine( MoveForwardCoroutine( 5f ) );
+		}
+	}
+
+	// move forward for "secondsToMove" duration of seconds
+	IEnumerator MoveForwardCoroutine(float secondsToMove) {
+		Debug.Log( "moveForwardCoroutine started!" );
+
+		if( isAlreadyMoving == true ) { // if we're already moving, end this coroutine instance
+			Debug.Log( "a coroutine is already running, so ending this coroutine early...");
+			yield break; // "yield break" ends a coroutine early
+		}
+
+		isAlreadyMoving = true; // when we begin, set this to "true" to prevent more than 1 coroutine from going
+
+		float t = 0f; // timer counter var to remember how long we've moved so far
+		while( t < secondsToMove ) {
+			t += Time.deltaTime; // add duration of frame to "t"
+			transform.Translate(0f, 0f, Time.deltaTime ); // move this object forward
+			yield return 0; // wait a frame at the end of each WHILE iteration
+		}
+
+		isAlreadyMoving = false; // when coroutine is done, set it back to "false"
+	}
+
+}
