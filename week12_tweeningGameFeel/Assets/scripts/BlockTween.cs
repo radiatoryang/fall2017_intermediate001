a56261bfd3e44342546027e6a5d58d5552369bd3
Robert Yang <yang.robert.w@gmail.com>
2017-11-21 13:34:18 -0500

    week12: tweening game feel (BlockTween.cs, Screenshake.cs)

---
+++ week12_tweeningGameFeel/Assets/scripts/BlockTween.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockTween : MonoBehaviour {
+
+	public AnimationCurve tweenCurve; // visually edit the tweening equation / tweening curve, instead of using math
+	Vector3 startPosition;
+	public Vector3 endPosition = new Vector3( 0f, -5f, 0f );
+
+	void Start () {
+		startPosition = transform.position;
+	}
+
+	void Update () {
+		if( Input.GetKeyDown( KeyCode.Space ) ) {
+			StartCoroutine( TweenCoroutine() );
+		}
+	}
+
+	IEnumerator TweenCoroutine () {
+		float time = 0f; // tracks time, from 0f to 1.0f (0% - 100%)
+		while( time < 1f ) {
+			time += Time.deltaTime; // increment time
+		 // transform.position = Vector3.Lerp( startPosition, endPosition, time);
+			transform.position = Vector3.Lerp( startPosition, endPosition, tweenCurve.Evaluate(time) );
+			yield return 0; // wait a frame! VERY IMPORTANT
+		}
+	}
+}
---
+++ week12_tweeningGameFeel/Assets/scripts/Screenshake.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// put this on your Main Camera
+// your Main Camera should be parented to a "CameraRig" type GameObject, and have localPos of (0,0,0)
+// (see demo scene)
+public class Screenshake : MonoBehaviour {
+
+	// "public static" = like a global variable, any script can access this var
+	// to access it, you'd write "Screenshake.shakeStrength" (you don't need GetComponent etc)
+	public static float shakeStrength = 0f; 
+	// now ANY script can shake the screen!
+	
+	// Update is called once per frame
+	void Update () {
+		// DEBUG TEST: press SPACE to shake the screen
+		if( Input.GetKeyDown( KeyCode.Space ) ) {
+			shakeStrength = 1f;
+		}
+
+		// here's where we actually shake the screen
+
+		// 1. calculate a shakeOffset
+		Vector3 shakeOffset = Vector3.right * Mathf.Sin( Time.time * 27f ) * 0.25f;
+		shakeOffset += Vector3.up * Mathf.Sin( Time.time * 37f ) * 0.25f;
+
+		// 2. apply shakeOffset to Camera's local position
+		transform.localPosition = shakeOffset * shakeStrength;
+
+		// 3. decay shakeStrength to always go back towards 0.0 (or else the screenshake never ends)
+		shakeStrength = Mathf.Clamp( shakeStrength - Time.deltaTime, 0f, 10f );
+	}
+}
